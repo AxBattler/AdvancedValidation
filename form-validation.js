2a58aadb7dc7546bfe0cca60769e7bc1317a3a97
Unknown <kgrager@mail.csuchico.edu>
2017-03-09 16:55:25 -0800

    added conditions and improved error feedback

--- form-validation.js
+++ form-validation.js
@@ -3,12 +3,18 @@ function validate(){
 	var userEntered = document.getElementById("user").value;
 	var passEntered = document.getElementById("pass").value;
 	// store length of username in var
-	var nameLength = userEntered.length;
-  
-  
+    
 	//show username error message if length is too short
-	if (nameLength < 6) {
-		document.getElementById("usernameError").innerHTML="Bad username.";
+	if (userEntered.length < 6) {
+		document.getElementById("usernameError").innerHTML="Username too short.";
+		document.getElementById("usernameError").classList.remove("hidden-message");
+		document.getElementById("usernameError").classList.add("shown-message");
+		//Turn the username items red
+		document.getElementById("usernameGroup").classList.add("has-error");
+	}
+	//show username error message if username contains a space
+	if (userEntered.indexOf(" ") > 0) {
+		document.getElementById("usernameError").innerHTML="Usernames can't contain spaces.";
 		document.getElementById("usernameError").classList.remove("hidden-message");
 		document.getElementById("usernameError").classList.add("shown-message");
 		//Turn the username items red
@@ -17,11 +23,26 @@ function validate(){
 	//otherwise flag successful username entry
 	else {
 		document.getElementById("usernameGroup").classList.add("has-success");
-		
 	}
 	//show password error message if user entered "password" as a password
-	if (passEntered.value == "password") {
-		document.getElementById("passwordError").innerHTML="Bad password.";
+	if (passEntered.toLowerCase() == "password") {
+		document.getElementById("passwordError").innerHTML="Password cannot be \"password\".";
+		document.getElementById("passwordError").classList.remove("hidden-message");
+		document.getElementById("passwordError").classList.add("shown-message");
+		//Turn the password items red
+		document.getElementById("passwordGroup").classList.add("has-error");
+	}
+	//show password error message if password length invalid
+	if (passEntered.length < 6 || passEntered.length > 20) {
+		document.getElementById("passwordError").innerHTML="Password must be between 6-20 characters.";
+		document.getElementById("passwordError").classList.remove("hidden-message");
+		document.getElementById("passwordError").classList.add("shown-message");
+		//Turn the password items red
+		document.getElementById("passwordGroup").classList.add("has-error");
+	}
+	//show password error message if password is the same as username
+	if (passEntered == userEntered) {
+		document.getElementById("passwordError").innerHTML="Password must be different from username.";
 		document.getElementById("passwordError").classList.remove("hidden-message");
 		document.getElementById("passwordError").classList.add("shown-message");
 		//Turn the password items red
